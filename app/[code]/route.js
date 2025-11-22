0513f5d1e2219b0699cbd2667ad90095497c9262
Abhishek Jha <sangam2788@gmail.com>
2025-11-22 19:43:33 +0530

    package.json and params conflict fix

--- app/[code]/route.js
+++ app/[code]/route.js
@@ -1,21 +1,18 @@
-import prisma from '@/lib/prisma';
-import { NextResponse } from 'next/server';
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
 
+export async function GET(req, context) {
+  const { code } = await context.params;
+  const link = await prisma.link.findUnique({ where: { code } });
+  if (!link) {
+    return NextResponse.json({ error: "URL Not found " }, { status: 404 });
+  }
 
-export async function GET(req, { params }) {
-const { code } = await params;
-const link = await prisma.link.findUnique({ where: { code } });
-if (!link) {
-return NextResponse.json({ error: 'URL Not found ' }, { status: 404 });
-}
+  // update clicks and lastClicked asynchronously but wait for it to succeed
+  await prisma.link.update({
+    where: { code },
+    data: { clicks: { increment: 1 }, lastClicked: new Date() },
+  });
 
-
-// update clicks and lastClicked asynchronously but wait for it to succeed
-await prisma.link.update({
-where: { code },
-data: { clicks: { increment: 1 }, lastClicked: new Date() }
-});
-
-
-return NextResponse.redirect(link.longUrl, 302);
+  return NextResponse.redirect(link.longUrl, 302);
 }
